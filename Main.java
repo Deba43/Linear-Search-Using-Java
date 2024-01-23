b402265362c7239cfbe501dc1d4b80b01e01b047
Debadatta Behera <105921307+Deba43@users.noreply.github.com>
2024-01-23 22:20:09 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int nums[] = {3, 7, 11, 99, 33};
+        int item  = 89;
+
+        int result = linearSearch(nums, item);
+        if(result != -1)
+            System.out.println("Element at index : " + result);
+        else
+            System.out.println("Element not found");
+
+    }
+
+    public static int linearSearch(int nums [], int item){
+        for(int i = 0; i < nums.length; i++){
+            if (nums[i] == item)
+                return i;
+        }
+        return - 1;
+    }
+}
